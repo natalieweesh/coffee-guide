5405950f0955fac417f8aadaea8840981bb1ddaa
natalieweesh <natalieweesh@gmail.com>
2014-04-22 01:01:48 -0400

    everything :neckbeard:

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/natalieweesh/pen/kpfGx.
+
+ a simple animated guide to coffee-making ways
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('.fixed.french').on('click', function(){
+  $('html, body').animate({scrollTop: $('#french-press').offset().top}, 1000);
+});
+
+$('.fixed.aero').on('click', function(){
+  $('html, body').animate({scrollTop: $('#aeropress').offset().top}, 1000);
+});
